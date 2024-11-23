70258bb0089280d5e23d24c80b7d87d63ce1017d
Pineapple Farmer <dlopez18@uncc.edu>
2024-11-22 21:11:39 -0500

    feat(auth): implement user authentication and event management
    - Add user registration with validation
    - Add login functionality with password comparison
    - Add profile view with user's events
    - Add logout functionality
    - Implement flash messages for user feedback
    - Add proper error handling and redirects
    - Update event controller with host field and authorization
    - Add flash messages to event operations
    - Update event model with host reference
    - Add user session management in app.js

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -3,154 +3,105 @@ const model = require('../models/event')
 
 
 // GET /events: send all events
-exports.index = (req, res, next) => {
-    model.find()
-        .then(events => {
-            let categories = [...new Set(events.map(event => event.category))]
-            res.render('./event/index', { events, categories });
-        })
-        .catch(err => { next(err) })
+exports.index = async (req, res, next) => {
+    try {
+        let events = await model.find().populate('host', 'firstName lastName');
+        res.render('./event/index', {events});
+    } catch(err) {
+        next(err);
+    }
 }
 
 // GET /events/create: send html form for creating new event
 exports.new = (req, res) => {
-    res.render('./event/newEvent')
+    res.render('./event/new')
 }
 
 // POST /events: create new event
-exports.create = (req, res, next) => {
-
-    // fix up req.body
-    req.body.category = req.body.category.charAt(0).toLowerCase() + req.body.category.slice(1)
-    req.body.startDateTime = new Date(`${req.body.when}T${req.body.start}`)
-    req.body.endDateTime = new Date(`${req.body.when}T${req.body.end}`)
-    req.body.image = '/images/' + req.file.filename
-    delete req.body.when
-    delete req.body.start
-    delete req.body.end
-
-    let event = new model(req.body)
-
-    event.save()
-        .then((event) => {
-            res.redirect('/events')
-        })
-        .catch(err => {
-            if (err.name === 'ValidationError') {
-                err.status = 400
-            }
-            next(err)
-        })
+exports.create = async (req, res, next) => {
+    try {
+        let event = new model(req.body);
+        event.host = req.session.user.id;  // Add host information
+        await event.save();
+        req.flash('success', 'Event created successfully');
+        res.redirect('/events');
+    } catch(err) {
+        if(err.name === 'ValidationError') {
+            req.flash('error', err.message);
+            return res.redirect('/back');
+        }
+        next(err);
+    }
 }
 
 // GET /events/:id: send details about event id
-exports.show = (req, res, next) => {
-    let id = req.params.id
-
-    // id needs to be a 24-bit hex string
-    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-        let err = new Error('Invalid event id')
-        err.status = 400
-        return next(err)
+exports.show = async (req, res, next) => {
+    try {
+        let id = req.params.id;
+        let event = await model.findById(id).populate('host', 'firstName lastName');
+        if(event) {
+            res.render('./event/show', {event});
+        } else {
+            let err = new Error('Cannot find a event with id ' + id);
+            err.status = 404;
+            next(err);
+        }
+    } catch(err) {
+        next(err);
     }
-
-    model.findById(id)
-        .then(event => {
-            if (event) {
-                res.render('./event/eventDetail', { event })
-            } else {
-                let err = new Error(`Cannot find a event with id ${id}`)
-                err.status = 404
-                next(err)
-            }
-        })
-        .catch(err => next(err))
 }
 
 // GET /events/:id/edit: send html form for editing existing event
-exports.edit = (req, res, next) => {
-    let id = req.params.id
-
-    // id needs to be a 24-bit hex string
-    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-        let err = new Error('Invalid event id')
-        err.status = 400
-        return next(err)
+exports.edit = async (req, res, next) => {
+    try {
+        let id = req.params.id;
+        let event = await model.findById(id);
+        if(event) {
+            res.render('./event/edit', {event});
+        } else {
+            let err = new Error('Cannot find a event with id ' + id);
+            err.status = 404;
+            next(err);
+        }
+    } catch(err) {
+        next(err);
     }
-
-    model.findById(id)
-        .then(event => {
-            if (event) {
-                res.render('./event/editEvent', { event })
-            } else {
-                let err = new Error(`Cannot find a event with id ${id}`)
-                err.status = 404
-                next(err)
-            }
-        })
-        .catch(err => next(err))
 }
 
 // PUT /events/:id: update event id
-exports.update = (req, res, next) => {
-    let event = req.body
-    let id = req.params.id
-    let img = req.file.filename
-
-    // fix up event
-    event.category = event.category.charAt(0).toLowerCase() + event.category.slice(1)
-    event.startDateTime = new Date(`${event.when}T${event.start}`)
-    event.endDateTime = new Date(`${event.when}T${event.end}`)
-    event.image = '/images/' + img
-    delete event.when
-    delete event.start
-    delete event.end
-
-    // id needs to be a 24-bit hex string
-    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-        let err = new Error('Invalid event id')
-        err.status = 400
-        return next(err)
+exports.update = async (req, res, next) => {
+    try {
+        let event = await model.findByIdAndUpdate(req.params.id, req.body, {runValidators: true});
+        if(event) {
+            req.flash('success', 'Event updated successfully');
+            res.redirect('/events/'+req.params.id);
+        } else {
+            let err = new Error('Cannot find a event with id ' + req.params.id);
+            err.status = 404;
+            next(err);
+        }
+    } catch(err) {
+        if(err.name === 'ValidationError') {
+            req.flash('error', err.message);
+            return res.redirect('/back');
+        }
+        next(err);
     }
-
-    model.findByIdAndUpdate(id, event, { useFindAndModify: false, runValidators: true })
-        .then(event => {
-            if (event) {
-                res.redirect(`/events/${id}`)
-            } else {
-                let err = new Error(`Cannot find a event with id ${id}`)
-                err.status = 404
-                next(err)
-            }
-        })
-        .catch(err => {
-            if (err.name === 'ValidationError') {
-                err.status = 400
-            }
-            next(err)
-        })
 }
 
 // DELETE /events/:id: delete event id
-exports.delete = (req, res, next) => {
-    let id = req.params.id
-
-    // id needs to be a 24-bit hex string
-    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-        let err = new Error('Invalid event id')
-        err.status = 400
-        return next(err)
+exports.delete = async (req, res, next) => {
+    try {
+        let event = await model.findByIdAndDelete(req.params.id);
+        if(event) {
+            req.flash('success', 'Event deleted successfully');
+            res.redirect('/events');
+        } else {
+            let err = new Error('Cannot find a event with id ' + req.params.id);
+            err.status = 404;
+            next(err);
+        }
+    } catch(err) {
+        next(err);
     }
-
-    model.findByIdAndDelete(id, { useFindAndModify: true })
-        .then(event => {
-            if (event) {
-                res.redirect('/events')
-            } else {
-                let err = new Error(`Cannot find event with id ${id}`)
-                err.status = 404
-                next(err)
-            }
-        })
-        .catch(err => next(err))
 }
